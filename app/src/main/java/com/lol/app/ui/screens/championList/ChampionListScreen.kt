91be2117c40c4bf6930b30eee44f9a7753c48b4a
JoaoMagalhaes <johnny.crq@gmail.com>
2026-05-21 20:53:24 +0100

    - removed list position reset on change sortOrder

--- app/src/main/java/com/lol/app/ui/screens/championList/ChampionListScreen.kt
+++ app/src/main/java/com/lol/app/ui/screens/championList/ChampionListScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -50,9 +49,6 @@ fun ChampionListScreen(
 ) {
 
   val listState = rememberLazyGridState()
-
-  LaunchedEffect(state.sortOrder) { listState.scrollToItem(0) }
-
   val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
   Scaffold(
